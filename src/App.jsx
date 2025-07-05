36f65d9fdfebe2f3d27fba4149852eaa74867c63
NayoanSk <pervesesk@gmail.com>
2025-07-05 10:14:11 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
       <div>
-        <h2 className="text-red-600">Bangladesh</h2>
+        <h2 className="text-red-600">This is from react project</h2>
       </div>
   )
 }
